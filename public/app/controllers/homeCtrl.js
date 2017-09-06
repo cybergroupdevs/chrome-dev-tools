878aff27bb6507d3a2f1555e8e024e7f815af097
Vishal Ranjan <vishivish18@gmail.com>
2017-09-07 02:01:39 +0530

    setting colors

--- public/app/controllers/homeCtrl.js
+++ public/app/controllers/homeCtrl.js
@@ -1,5 +1,12 @@
 angular.module('app')
     .controller('homeCtrl', function($scope, $http) {
+        window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+        window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+        window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+
+        if (!window.indexedDB) {
+            window.alert("Your browser doesn't support a stable version of IndexedDB.")
+        }
         console.log("hello")
         $scope.button = "Remove"
         $scope.showElement = true;
@@ -36,6 +43,26 @@ angular.module('app')
             })
         }
 
+        const employeeData = [
+            { id: "01", name: "Vishal R", age: 25, email: "me@vishalranjan.in" },
+            { id: "02", name: "Tony Stark", age: 34, email: "tony@stark.com" }
+        ];
+
+        $scope.add = function() {
+            var request = db.transaction(["employee"], "readwrite")
+                .objectStore("employee")
+                .add({ id: "01", name: "prasad", age: 24, email: "prasad@tutorialspoint.com" });
+
+            request.onsuccess = function(event) {
+                alert("Prasad has been added to your database.");
+            };
+
+            request.onerror = function(event) {
+                alert("Unable to add data\r\nPrasad is already exist in your database! ");
+            }
+        }
+
+        //$scope.add();
 
 
     })
